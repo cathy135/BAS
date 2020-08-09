0e28001731ddf4b560b8c8360afc914e9e55debb
Cathy Lee <cathylee@MacBook-Pro.fios-router.home>
2020-08-09 17:10:45 -0400

    complete modification of hyperg.c with mpfr

--- src/mp_hyperg.c
+++ src/mp_hyperg.c
@@ -134,21 +134,24 @@ static double mphy1f1p( a, b, x, err )
   double a, b, x;
 double *err;
 {
-  
-  
-  double n, t, temp, an, bn, pcanc, u_d;
-  mpfr_t u, a0, sum, maxt, res; 
+  double n, t, temp, an, bn, pcanc;
+  mpfr_t x1, u, a0, sum, maxt, resp; 
   
   an = a;
   bn = b;
   n = 1.0;
   t = 1.0;
   
-  mpfr_init2 (u, 200);
-  mpfr_init_set_d (a0, 1.0, 200);
-  mpfr_init_set_d (sum, 1.0, 200);
-  mpfr_init_set_d (maxt, 0.0, 200);
-  mpfr_init2 (res, 200);
+  mpfr_init2(u, 200);
+  mpfr_init2(x1, 200);
+  mpfr_set_d(x1, x, MPFR_RNDN);
+  mpfr_init2(a0, 200);
+  mpfr_set_d(a0, 1.0, MPFR_RNDN);
+  mpfr_init2(sum, 200);
+  mpfr_set_d(sum, 1.0, MPFR_RNDN);
+  mpfr_init2(maxt, 200);
+  mpfr_set_d(maxt, 0.0, MPFR_RNDN);
+  mpfr_init2(resp, 200);
   
   while( t > MACHEP )
   {
@@ -161,9 +164,8 @@ double *err;
       return( mpfr_get_d(sum, MPFR_RNDN) );
     if( n > 200 ) 
       goto pdone;
-    
-    u_d = x * (an / (bn * n));
-    mpfr_set_d (u, u_d, MPFR_RNDN);
+      
+    mpfr_mul_d(u, x1, (an / (bn * n)), MPFR_RNDN);  
       
     /* check for blowup */
     temp = fabs(mpfr_get_d(u, MPFR_RNDN));
@@ -173,8 +175,8 @@ double *err;
       goto blowup;
     } 
     
-    mpfr_mul (a0, a0, u, MPFR_RNDN); 
-    mpfr_add (sum, sum, a0, MPFR_RNDN);
+    mpfr_mul(a0, a0, u, MPFR_RNDN); 
+    mpfr_add(sum, sum, a0, MPFR_RNDN);
     
     t = fabs(mpfr_get_d(a0, MPFR_RNDN));
     if (t > mpfr_get_d(maxt, MPFR_RNDN)) {
@@ -190,23 +192,24 @@ double *err;
     
     /* estimate error due to roundoff and cancellation */
     if( mpfr_get_d(sum, MPFR_RNDN) != 0.0 )
-      mpfr_div_d (maxt, maxt, fabs(mpfr_get_d(sum, MPFR_RNDN)), MPFR_RNDN);
+      mpfr_div_d(maxt, maxt, fabs(mpfr_get_d(sum, MPFR_RNDN)), MPFR_RNDN);
     
-    mpfr_mul_d (maxt, maxt, MACHEP, MPFR_RNDN);   /* this way avoids multiply overflow */
+    mpfr_mul_d(maxt, maxt, MACHEP, MPFR_RNDN);   /* this way avoids multiply overflow */
     pcanc = fabs( MACHEP * n  +  mpfr_get_d(maxt, MPFR_RNDN) );
     
     blowup:
       
       *err = pcanc;
     
-    mpfr_set (res, sum, MPFR_RNDN);
+    mpfr_set(resp, sum, MPFR_RNDN);
     
-    mpfr_clear (u);
-    mpfr_clear (a0);
-    mpfr_clear (sum);
-    mpfr_clear (maxt);
+    mpfr_clear(x1);
+    mpfr_clear(u);
+    mpfr_clear(a0);
+    mpfr_clear(sum);
+    mpfr_clear(maxt);
     
-    return( mpfr_get_d(res, MPFR_RNDN) ); 
+    return( mpfr_get_d(resp, MPFR_RNDN) ); 
     
 }
 
@@ -229,171 +232,209 @@ double *err;
  */
 
 static double mphy1f1a( a, b, x, err )
-double a, b, x;
+  double a, b, x;
 double *err;
 {
-double h1, h2, t, u, temp, acanc, asum, err1, err2;
-
-if( x == 0 )
-	{
-	acanc = 1.0;
-	asum = MAXNUM;
-	goto adone;
-	}
-temp = log( fabs(x) );
-t = x + temp * (a-b);
-u = -temp * a;
-
-if( b > 0 )
-	{
-	temp = lgam(b);
-	t += temp;
-	u += temp;
-	}
-
-h1 = hyp2f0( a, a-b+1, -1.0/x, 1, &err1 );
-
-temp = exp(u) / gam(b-a);
-h1 *= temp;
-err1 *= temp;
-
-h2 = hyp2f0( b-a, 1.0-a, 1.0/x, 2, &err2 );
-
-if( a < 0 )
-	temp = exp(t) / gam(a);
-else
-	temp = exp( t - lgam(a) );
-
-h2 *= temp;
-err2 *= temp;
-
-if( x < 0.0 )
-	asum = h1;
-else
-	asum = h2;
-
-acanc = fabs(err1) + fabs(err2);
-
-
-if( b < 0 )
-	{
-	temp = gam(b);
-	asum *= temp;
-	acanc *= fabs(temp);
-	}
-
-
-if( asum != 0.0 )
-	acanc /= fabs(asum);
-
-acanc *= 30.0;	/* fudge factor, since error of asymptotic formula
-		 * often seems this much larger than advertised */
+  
+  double t, err1, err2, acanc;
+  mpfr_t h1, h2, u, temp, asum, resa;
+  
+  mpfr_init2(h1, 200);
+  mpfr_init2(h2, 200);
+  mpfr_init2(u, 200);
+  mpfr_init2(temp, 200);
+  mpfr_init2(asum, 200);
+  mpfr_init2(resa, 200);
+  
+  if( x == 0 )
+  {
+    acanc = 1.0;
+    mpfr_set_d(asum, MAXNUM, MPFR_RNDN);
+    goto adone;
+  }
+  mpfr_set_d(temp, log(fabs(x)), MPFR_RNDN);
+  t = x + mpfr_get_d(temp, MPFR_RNDN) * (a-b);
+  mpfr_set_d(u, -1 * mpfr_get_d(temp, MPFR_RNDN) * a, MPFR_RNDN);
+  
+  if( b > 0 )
+  {
+    mpfr_set_d(temp, lgam(b), MPFR_RNDN);
+    t += mpfr_get_d(temp, MPFR_RNDN);
+    mpfr_add(u, u, temp, MPFR_RNDN);
+  }
+  
+  mpfr_set_d(h1, hyp2f0( a, a-b+1, -1.0/x, 1, &err1 ), MPFR_RNDN);
+  
+  mpfr_set_d(temp, exp(mpfr_get_d(u, MPFR_RNDN))/gam(b-a), MPFR_RNDN);
+  mpfr_mul(h1, h1, temp, MPFR_RNDN);
+  err1 *= mpfr_get_d(temp, MPFR_RNDN);
+  
+  mpfr_set_d(h2, hyp2f0( b-a, 1.0-a, 1.0/x, 2, &err2 ), MPFR_RNDN);
+  
+  if( a < 0 )
+    mpfr_set_d(temp, exp(t) / gam(a), MPFR_RNDN);
+  else
+    mpfr_set_d(temp, exp( t - lgam(a) ), MPFR_RNDN);
+  
+  mpfr_mul(h2, h2, temp, MPFR_RNDN);
+  err2 *= mpfr_get_d(temp, MPFR_RNDN);
+  
+  
+  if( x < 0.0 )
+    mpfr_set(asum, h1, MPFR_RNDN);
+  else
+    mpfr_set(asum, h2, MPFR_RNDN);
+  
+  acanc = fabs(err1) + fabs(err2);
+  
+  if( b < 0 )
+  {
+    mpfr_set_d(temp, gam(b), MPFR_RNDN);
+    mpfr_mul(asum, asum, temp, MPFR_RNDN);
+    acanc *= fabs(mpfr_get_d(temp, MPFR_RNDN));
+  }
+  
+  
+  if( mpfr_get_d(asum, MPFR_RNDN) != 0.0 )
+    acanc /= fabs(mpfr_get_d(asum, MPFR_RNDN));
+  
+  
+  acanc *= 30.0;	/* fudge factor, since error of asymptotic formula
+ * often seems this much larger than advertised */
 
 adone:
+  
+  *err = acanc;
 
+mpfr_set(resa, asum, MPFR_RNDN);
 
-*err = acanc;
-return( asum );
+mpfr_clear(asum);
+mpfr_clear(temp);
+mpfr_clear(h1);
+mpfr_clear(h2);
+mpfr_clear(u);
+
+return( mpfr_get_d(resa, MPFR_RNDN) );
 }
 
 /*							hyp2f0()	*/
 
 double mphyp2f0( a, b, x, type, err )
-double a, b, x;
+  double a, b, x;
 int type;	/* determines what converging factor to use */
 double *err;
 {
-double a0, alast, t, tlast, maxt;
-double n, an, bn, u, sum, temp;
-
-an = a;
-bn = b;
-a0 = 1.0e0;
-alast = 1.0e0;
-sum = 0.0;
-n = 1.0e0;
-t = 1.0e0;
-tlast = 1.0e9;
-maxt = 0.0;
-
-do
-	{
-	if( an == 0 )
-		goto pdone;
-	if( bn == 0 )
-		goto pdone;
-
-	u = an * (bn * x / n);
-
-	/* check for blowup */
-	temp = fabs(u);
-	if( (temp > 1.0 ) && (maxt > (MAXNUM/temp)) )
-		goto error;
-
-	a0 *= u;
-	t = fabs(a0);
-
-	/* terminating condition for asymptotic series */
-	if( t > tlast )
-		goto ndone;
-
-	tlast = t;
-	sum += alast;	/* the sum is one term behind */
-	alast = a0;
-
-	if( n > 200 )
-		goto ndone;
-
-	an += 1.0e0;
-	bn += 1.0e0;
-	n += 1.0e0;
-	if( t > maxt )
-		maxt = t;
-	}
-while( t > MACHEP );
-
-
-pdone:	/* series converged! */
-
-/* estimate error due to roundoff and cancellation */
-*err = fabs(  MACHEP * (n + maxt)  );
-
-alast = a0;
-goto done;
-
-ndone:	/* series did not converge */
-
-/* The following "Converging factors" are supposed to improve accuracy,
- * but do not actually seem to accomplish very much. */
-
-n -= 1.0;
-x = 1.0/x;
-
-switch( type )	/* "type" given as subroutine argument */
-{
-case 1:
-	alast *= ( 0.5 + (0.125 + 0.25*b - 0.5*a + 0.25*x - 0.25*n)/x );
-	break;
-
-case 2:
-	alast *= 2.0/3.0 - b + 2.0*a + x - n;
-	break;
-
-default:
-	;
-}
-
-/* estimate error due to roundoff, cancellation, and nonconvergence */
-*err = MACHEP * (n + maxt)  +  fabs ( a0 );
-
-
-done:
-sum += alast;
-return( sum );
-
-/* series blew up: */
-error:
-*err = MAXNUM;
-mtherr( "hyperg", TLOSS );
-return( sum );
+  double n, an, bn, t, tlast, temp;
+  mpfr_t x1, a0, alast, u, sum, maxt, res0;
+  
+  an = a;
+  bn = b;
+  n = 1.0e0;
+  t = 1.0e0;
+  tlast = 1.0e9;
+  
+  mpfr_init2(x1, 200);
+  mpfr_init2(u, 200);
+  mpfr_init2(a0, 200);
+  mpfr_set_d(a0, 1.0e0, MPFR_RNDN);
+  mpfr_init2(alast, 200);
+  mpfr_set_d(alast, 1.0e0, MPFR_RNDN);
+  mpfr_init2(sum, 200);
+  mpfr_set_d(sum, 0.0, MPFR_RNDN);
+  mpfr_init2(maxt, 200);
+  mpfr_set_d(maxt, 0.0, MPFR_RNDN);
+  mpfr_init2(res0, 200);
+  
+  do
+  {
+    if( an == 0 )
+      goto pdone;
+    if( bn == 0 )
+      goto pdone;
+    
+    mpfr_mul_d(u, x1, an * bn/n, MPFR_RNDN);
+    
+    /* check for blowup */
+    temp = fabs(mpfr_get_d(u, MPFR_RNDN));
+    if( (temp > 1.0 ) && (mpfr_get_d(maxt, MPFR_RNDN) > (MAXNUM/temp)) )
+      goto error;
+    
+    mpfr_mul(a0, a0, u, MPFR_RNDN); 
+    t = fabs(mpfr_get_d(a0, MPFR_RNDN));
+    
+    /* terminating condition for asymptotic series */
+    if( t > tlast )
+      goto ndone;
+    
+    tlast = t;
+    mpfr_add(sum, sum, alast, MPFR_RNDN);  /* the sum is one term behind */
+    mpfr_set(alast, a0, MPFR_RNDN);
+    
+    if( n > 200 )
+      goto ndone;
+    
+    an += 1.0e0;
+    bn += 1.0e0;
+    n += 1.0e0;
+    if( t > mpfr_get_d(maxt, MPFR_RNDN) )
+      mpfr_set_d(maxt, t, MPFR_RNDN);
+  }
+  while( t > MACHEP );
+  
+  
+  pdone:	/* series converged! */
+    
+    /* estimate error due to roundoff and cancellation */
+    *err = fabs(  MACHEP * (n + mpfr_get_d(maxt, MPFR_RNDN))  );
+    
+    mpfr_set(alast, a0, MPFR_RNDN);
+    goto done;
+    
+    ndone:	/* series did not converge */
+    
+    /* The following "Converging factors" are supposed to improve accuracy,
+     * but do not actually seem to accomplish very much. */
+    
+    n -= 1.0;
+    x = 1.0/x;
+    
+    switch( type )	/* "type" given as subroutine argument */
+    {
+    case 1:
+      mpfr_mul_d(alast, alast, ( 0.5 + (0.125 + 0.25*b - 0.5*a + 0.25*x - 0.25*n)/x ), MPFR_RNDN);
+      break;
+      
+    case 2:
+      mpfr_mul_d(alast, alast, 2.0/3.0 - b + 2.0*a + x - n, MPFR_RNDN);
+      break;
+      
+    default:
+      ;
+    }
+    
+    /* estimate error due to roundoff, cancellation, and nonconvergence */
+    *err = MACHEP * (n + mpfr_get_d(maxt, MPFR_RNDN))  +  fabs ( mpfr_get_d(a0, MPFR_RNDN) );
+    
+    
+    done:
+      mpfr_add(sum, sum, alast, MPFR_RNDN);
+    
+    mpfr_set(res0, sum, MPFR_RNDN);
+    
+    mpfr_clear(a0);
+    mpfr_clear(alast);
+    mpfr_clear(u);
+    mpfr_clear(sum);
+    mpfr_clear(maxt);
+    
+    return( mpfr_get_d(res0, MPFR_RNDN) );
+    
+    /* series blew up: */
+    error:
+      *err = MAXNUM;
+    mtherr( "hyperg", TLOSS );
+    
+    mpfr_set(res0, sum, MPFR_RNDN);
+    
+    return( mpfr_get_d(res0, MPFR_RNDN) );
 }
